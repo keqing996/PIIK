85ce8eac286ae30653e7dbc9f5967856d2e3f7ca
keqing996 <epigrf@outlook.com>
2024-01-21 15:42:14 +0800

    Win OpenGL support

--- src/Helper/Ext/OpenGL.h
+++ src/Helper/Ext/OpenGL.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// Cmake: find_package(OpenGL REQUIRED)
+// Link: ${OPENGL_LIBRARIES}
+// Windows Kits\10\Lib\10.0.xxx.0\um\x64\OpenGL32.Lib
+
 #ifdef HELPER_EXT_OPENGL
 
 namespace Helper::Ext::OpenGL
