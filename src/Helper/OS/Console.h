a9ad0792b948560ee456dde85c04d685bb25c170
keqing996 <epigrf@outlook.com>
2024-01-29 14:05:07 +0800

    Update windows console

--- src/Helper/OS/Console.h
+++ src/Helper/OS/Console.h
@@ -4,5 +4,31 @@
 
 namespace Helper::OS
 {
+    class Console
+    {
+    public:
+        enum class Color
+        {
+            None,
+            Green,
+            Red,
+            Blue,
+            White,
+            Black,
+            Yellow,
+            Purple,
+            Cyan
+        };
 
+    public:
+        Console() = delete;
+
+    public:
+        static void CreateConsole();
+        static void AttachConsole();
+        static void DetachConsole();
+        static void SetConsoleOutputUtf8();
+        static void SetStdOutColor(Color foreground, Color background, bool foregroundIntensity = false, bool backgroundIntensity = false);
+        static void ClearScreen();
+    };
 }
