76f62dd944fa433ebfefd22dcae97a30137b3363
keqing996 <epigrf@outlook.com>
2024-01-31 19:42:35 +0800

    Update window

---
+++ src/Helper/OS/Window.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../PlatformDefine.h"
+
+namespace Helper::OS
+{
+    class Window
+    {
+    public:
+        Window() = delete;
+
+    public:
+
+    };
+}
---
+++ src/Helper/OS/Windows/Window.cpp
@@ -0,0 +1,11 @@
+#include "../Window.h"
+#include "WindowsDefine.h"
+
+#if PLATFORM_WINDOWS
+
+namespace Helper::OS
+{
+
+}
+
+#endif
