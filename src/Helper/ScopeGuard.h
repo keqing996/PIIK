7dd2e483b4370d434e715943cf30d27cf84c6b8c
keqing996 <epigrf@outlook.com>
2024-01-31 19:11:43 +0800

    Scope guard

---
+++ src/Helper/ScopeGuard.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <functional>
+#include "NonCopyable.h"
+
+namespace Helper
+{
+    class ScopeGuard : public NonCopyable
+    {
+    public:
+        ScopeGuard(ScopeGuard&& other) = delete;
+
+        ~ScopeGuard()
+        {
+            _cleanup();
+        }
+
+        template<class Func>
+        ScopeGuard(const Func& cleanup) // NOLINT(*-explicit-constructor)
+            : _cleanup(cleanup)
+        {
+        }
+
+    private:
+        std::function<void()> _cleanup;
+    };
+}
