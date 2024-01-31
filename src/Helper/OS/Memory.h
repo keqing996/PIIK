bb1620deffe9b741ec96fd7fb380a5a9dd7a6560
keqing996 <epigrf@outlook.com>
2024-01-31 17:09:54 +0800

    Update memory

---
+++ src/Helper/OS/Memory.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../PlatformDefine.h"
+
+namespace Helper::OS
+{
+    class Memory
+    {
+    public:
+        Memory() = delete;
+
+    public:
+        static void* VirtualReserve(void* wantedAddr, size_t size);
+        static bool VirtualCommit(void* addr, size_t size);
+        static void VirtualRelease(void* addr);
+        static size_t CurrentPageSize();
+    };
+}
