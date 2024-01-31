f339af04874a3d816ea0e55ee24d3d43f70d5c2a
keqing996 <epigrf@outlook.com>
2024-01-31 17:11:50 +0800

    Update memory

--- src/Helper/WinApi/WinApiMemory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-namespace Helper::Win::Memory
-{
-    void* VirtualMemoryReserve(void* wantedAddress, size_t size);
-    bool VirtualMemoryCommit(void* address, size_t size);
-    void* VirtualMemoryRelease(void* address, size_t size);
-}
