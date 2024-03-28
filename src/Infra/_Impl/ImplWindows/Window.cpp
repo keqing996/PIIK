64fb3546410d932bd46000e65ee264c4b168949d
keqing996 <epigrf@outlook.com>
2024-03-28 11:22:04 +0800

    Window dpi aware.

--- src/Infra/_Impl/ImplWindows/Window.cpp
+++ src/Infra/_Impl/ImplWindows/Window.cpp
@@ -3,19 +3,91 @@
 
 #if PLATFORM_WINDOWS
 
+#include "../../ScopeGuard.h"
 #include "../../String.h"
+#include "../../Logger.h"
 
 namespace Infra
 {
-    struct Window::WindowHandle
+    class Support
     {
-        HWND hWnd;
+    public:
+        Support() = delete;
+
+    public:
+        static bool FixProcessDpi()
+        {
+            // Try SetProcessDpiAwareness first
+            if (FixProcessDpiBySetProcessDpiAwareness())
+                return true;
+
+            // Fall back to SetProcessDPIAware
+            if (FixProcessDpiBySetProcessDPIAware())
+                return true;
+
+            return false;
+        }
+
+    private:
+        static bool FixProcessDpiBySetProcessDpiAwareness()
+        {
+            HINSTANCE pShcodeDll = ::LoadLibraryW(L"Shcore.dll");
+
+            if (pShcodeDll == nullptr)
+                return false;
+
+            ScopeGuard pShcodeDllReleaseGuard = [&pShcodeDll]()
+            {
+                ::FreeLibrary(pShcodeDll);
+            };
+
+            void* pFuncSetProcessDpiAwareness = reinterpret_cast<void*>(GetProcAddress(pShcodeDll, "SetProcessDpiAwareness"));
+            if (pFuncSetProcessDpiAwareness == nullptr)
+                return false;
+
+            enum ProcessDpiAwareness
+            {
+                ProcessDpiUnaware         = 0,
+                ProcessSystemDpiAware     = 1,
+                ProcessPerMonitorDpiAware = 2
+            };
+
+            using SetProcessDpiAwarenessFuncType = HRESULT (WINAPI*)(ProcessDpiAwareness);
+
+            auto setProcessDpiAwarenessFunc = reinterpret_cast<SetProcessDpiAwarenessFuncType>(pFuncSetProcessDpiAwareness);
+            if (setProcessDpiAwarenessFunc(ProcessPerMonitorDpiAware) == E_INVALIDARG)
+                return false;
+
+            return true;
+        }
+
+        static bool FixProcessDpiBySetProcessDPIAware()
+        {
+            HINSTANCE pUser32Dll = ::LoadLibraryW(L"user32.dll");
+
+            if (pUser32Dll == nullptr)
+                return false;
+
+            ScopeGuard pShcodeDllReleaseGuard = [&pUser32Dll]()
+            {
+                ::FreeLibrary(pUser32Dll);
+            };
+
+            void* pFuncSetProcessDPIAware = reinterpret_cast<void*>(GetProcAddress(pUser32Dll, "SetProcessDPIAware"));
+            if (pFuncSetProcessDPIAware == nullptr)
+                return false;
+
+            using SetProcessDPIAwareFuncType = BOOL (WINAPI*)(void);
+
+            auto setProcessDpiAwareFunc = reinterpret_cast<SetProcessDPIAwareFuncType>(pFuncSetProcessDPIAware);
+            if (!setProcessDpiAwareFunc())
+                return false;
+
+            return true;
+        }
     };
 
-    struct Window::DeviceContextHandle
-    {
-        HDC hDeviceContext;
-    };
+
 
 
 }
