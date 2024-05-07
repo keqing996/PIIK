b65c29fa537ff3bfb0eec3532900d6846277f1bf
keqing996 <epigrf@outlook.com>
2024-05-07 11:36:03 +0800

    ImGuiWinApp example.

--- src/ImGuiWinApp/ImGuiWinApp.cpp
+++ src/ImGuiWinApp/ImGuiWinApp.cpp
@@ -1,8 +1,10 @@
 #include <Infra/Windows/WindowsDefine.hpp>
 #include <imgui.h>
-#include "ImGuiWinApp.h"
 #include <backends/imgui_impl_win32.h>
 #include <backends/imgui_impl_dx11.h>
+#include "ImGuiWinApp.h"
+
+#pragma comment(lib, "d3d11.lib")
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
