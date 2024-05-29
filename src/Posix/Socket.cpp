b1207a28b6427f67396e5a5a6141ea31328beebd
keqing996 <epigrf@outlook.com>
2024-05-29 13:55:38 +0800

    Socket update.

--- src/Posix/Socket.cpp
+++ src/Posix/Socket.cpp
@@ -1,4 +1,4 @@
-#include "Infra/Windows/WindowsDefine.hpp"
+#include "Infra/PlatformDefine.hpp"
 
 #if PLATFORM_SUPPORT_POSIX
 
@@ -19,8 +19,43 @@
 
 namespace Infra
 {
-    std::optional<Socket::EndPoint<Socket::AddressFamily::IpV4>> Socket::EndPoint<Socket::AddressFamily::IpV4>::TryCreate(const std::string& ip, uint16_t port)
+
+
+    auto Socket::IsInitialized() -> bool
     {
+        return true;
+    }
+
+    auto Socket::InitEnvironment() -> bool
+    {
+        // do nothing
+        return true;
+    }
+
+    auto Socket::DestroyEnvironment() -> void
+    {
+        // do nothing
+    }
+
+    auto Socket::CreateEndpoint(const std::string& ipStr, uint16_t port) -> std::optional<EndPoint<AddressFamily::IpV4>>
+    {
+        char buffer[INET_ADDRSTRLEN];
+        int ret = ::inet_pton(AF_INET, ipStr.c_str(), buffer);
+        if (ret <= 0)
+            return std::nullopt;
+
+        uint32_t ip = std::stoul(buffer);
+        return EndPoint<AddressFamily::IpV4>(ip, port);
+    }
+
+    auto Socket::CreateSocket(AddressFamily family, Protocol protocol) -> std::optional<SocketHandle>
+    {
+
+    }
+
+    auto Socket::DestroySocket(const SocketHandle& socketHandle) -> void
+    {
+
     }
 }
 
