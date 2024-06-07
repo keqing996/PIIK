f069662bf9bb6a848d2d9635863fc763c409034b
keqing996 <epigrf@outlook.com>
2024-06-07 21:06:31 +0800

    Update command line.

--- include/Infra/CommandLine.hpp
+++ include/Infra/CommandLine.hpp
@@ -1,9 +1,100 @@
 #pragma once
 
+#include <string>
+
 namespace Infra
 {
     class CommandLine
     {
-        
+    public:
+        CommandLine() = default;
+
+    private:
+        class Option
+        {
+        public:
+            Option(const std::string& name, char shortName, const std::string& desc)
+                : _fullName(name)
+                , _shortName(shortName)
+                , _desc(desc)
+            {
+            }
+
+        public:
+            const std::string& GetFullName()
+            {
+                return _fullName;
+            }
+
+            char GetShortName()
+            {
+                return _shortName;
+            }
+
+            const std::string GetDesc()
+            {
+                return _desc;
+            }
+
+        public:
+            virtual bool HasValue() = 0;
+            virtual bool Settle() = 0;
+
+        private:
+            std::string _fullName;
+            char _shortName;
+            std::string _desc;
+        };
+
+        class OptionNoValue: Option
+        {
+        public:
+            OptionNoValue(const std::string& name, char shortName, const std::string& desc)
+                : Option(name, shortName, desc)
+                , _settle(false)
+            {
+            }
+
+        public:
+            bool HasValue() override
+            {
+                return false;
+            }
+
+            bool Set()
+            {
+                _settle = true;
+                return true;
+            }
+
+            bool Settle() override
+            {
+                return _settle;
+            }
+
+        private:
+            bool _settle;
+        };
+
+        template<typename T>
+        class OptionWithValue : Option
+        {
+        public:
+
+
+        public:
+
+        private:
+
+        };
+
+    public:
+        void AddOption(const std::string& fullName, char shortName, const std::string& desc);
+
+        template<typename T>
+        void AddOption(const std::string& fullName, char shortName, const std::string& desc)
+
+    private:
+
     };
 }
