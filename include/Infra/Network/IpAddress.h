caabeb95451ed75149ac568d2912e42ba18ff9b6
keqing996 <epigrf@outlook.com>
2024-07-30 11:11:04 +0800

    Update.

--- include/Infra/Network/IpAddress.h
+++ include/Infra/Network/IpAddress.h
@@ -27,8 +27,8 @@ namespace Infra
         explicit IpAddress(const uint8_t* pAddr);
 
         // operator
-        bool operator==(const IpAddress& left, const IpAddress& right) const;
-        bool operator!=(const IpAddress& left, const IpAddress& right) const;
+        friend bool operator==(const IpAddress& left, const IpAddress& right);
+        friend bool operator!=(const IpAddress& left, const IpAddress& right);
 
     public:
         Family GetFamily() const;
@@ -52,55 +52,6 @@ namespace Infra
         IpData _data;
     };
 
-    /*
-    template<>
-    class IpAddress<AddressFamily::IpV4>
-    {
-    public:
-        IpAddress();
-        explicit IpAddress(uint32_t hostOrderIp);
-        IpAddress(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4);
-
-        bool operator==(const IpAddress& left, const IpAddress& right) const;
-        bool operator!=(const IpAddress& left, const IpAddress& right) const;
-
-    public:
-        std::string ToString() const;
-
-    public:
-        static IpAddress LOCAL_HOST;
-        static IpAddress ANY;
-        static IpAddress BROADCAST;
-
-        static std::optional<IpAddress> TryParse(const std::string& str);
-
-    private:
-        // network order
-        uint32_t _address;
-    };
-
-    template<>
-    class IpAddress<AddressFamily::IpV6>
-    {
-    public:
-        explicit IpAddress(const uint8_t* pAddr);
-
-        bool operator==(const IpAddress& left, const IpAddress& right) const;
-        bool operator!=(const IpAddress& left, const IpAddress& right) const;
-
-    public:
-        std::string ToString() const;
-
-    public:
-        static int constexpr ADDR_SIZE_BYTE = 16;
-        static std::optional<IpAddress> TryParse(const std::string& str);
-
-    private:
-        // network order
-        std::array<uint8_t, ADDR_SIZE_BYTE> _address{};
-    };
-
-    using IpV4 = IpAddress<AddressFamily::IpV4>;
-    using IpV6 = IpAddress<AddressFamily::IpV6>;
-    */
+    bool operator==(const IpAddress& left, const IpAddress& right);
+    bool operator!=(const IpAddress& left, const IpAddress& right);
 }
--- src/Network/IpAddress.cpp
+++ src/Network/IpAddress.cpp
@@ -7,120 +7,20 @@
 
 namespace Infra
 {
-
     IpAddress IpAddress::V4_ANY (0, 0, 0, 0);
     IpAddress IpAddress::V4_BROADCAST (255, 255, 255, 255);
     IpAddress IpAddress::V4_LOCAL_HOST (127, 0,0 , 1);
 
-
-
-    /*
-    IpAddress<AddressFamily::IpV4>::IpAddress LOCAL_HOST (127, 0, 0, 1);
-    IpAddress<AddressFamily::IpV4>::IpAddress ANY (0, 0, 0, 0);
-    IpAddress<AddressFamily::IpV4>::IpAddress BROADCAST (255, 255, 255, 255);
-
-    IpAddress<AddressFamily::IpV4>::IpAddress() : _address(0)
-    {
-    }
-
-    IpAddress<AddressFamily::IpV4>::IpAddress(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4)
-    {
-        _address = static_cast<uint32_t>(byte1) << 24;
-        _address |= static_cast<uint32_t>(byte2) << 16;
-        _address |= static_cast<uint32_t>(byte3) << 8;
-        _address |= static_cast<uint32_t>(byte4);
-        _address = ::htonl(_address);
-    }
-
-    IpAddress<AddressFamily::IpV4>::IpAddress(uint32_t hostOrderIp)
-    {
-        _address = ::htonl(hostOrderIp);
-    }
-
-    std::optional<IpAddress<AddressFamily::IpV4>> IpAddress<AddressFamily::IpV4>::TryParse(const std::string& str)
-    {
-        in_addr destinationIP {};
-        auto result = ::inet_pton(AF_INET, str.c_str(), &destinationIP);
-        if (result != 1)
-            return std::nullopt;
-
-        IpAddress ip;
-        ip._address = destinationIP.s_addr;
-        return ip;
-    }
-
-    std::optional<IpAddress<AddressFamily::IpV6>> IpAddress<AddressFamily::IpV6>::TryParse(const std::string& str)
-    {
-        in6_addr destinationIP {};
-        auto result = ::inet_pton(AF_INET6, str.c_str(), &destinationIP);
-        if (result != 1)
-            return std::nullopt;
-
-        return IpAddress{ reinterpret_cast<const uint8_t*>(&destinationIP.s6_addr) };
-    }
-
-    std::string IpAddress<AddressFamily::IpV4>::ToString() const
-    {
-        in_addr address{};
-        address.s_addr = _address;
-
-        char resultBuffer[INET_ADDRSTRLEN];
-
-        ::inet_ntop(AF_INET, &address, resultBuffer, INET_ADDRSTRLEN);
-
-        return resultBuffer;
-    }
-
-    std::string IpAddress<AddressFamily::IpV6>::ToString() const
-    {
-        in6_addr address{};
-
-        ::memcpy(&address.s6_addr, _address.data(), ADDR_SIZE_BYTE);
-
-        char resultBuffer[INET6_ADDRSTRLEN];
-
-        ::inet_ntop(AF_INET, &address, resultBuffer, INET6_ADDRSTRLEN);
-
-        return resultBuffer;
-    }
-
-    bool IpAddress<AddressFamily::IpV4>::operator==(const IpAddress& left, const IpAddress& right) const
-    {
-        return left._address == right._address;
-    }
-
-    bool IpAddress<AddressFamily::IpV4>::operator!=(const IpAddress& left, const IpAddress& right) const
-    {
-        return left._address != right._address;
-    }
-
-    IpAddress<AddressFamily::IpV6>::IpAddress(const uint8_t* pAddr)
-    {
-        ::memcpy(_address.data(), pAddr, ADDR_SIZE_BYTE);
-    }
-
-    bool IpAddress<AddressFamily::IpV6>::operator==(const IpAddress& left, const IpAddress& right) const
-    {
-        return left._address == right._address;
-    }
-
-    bool IpAddress<AddressFamily::IpV6>::operator!=(const IpAddress& left, const IpAddress& right) const
-    {
-        return left._address != right._address;
-    }
-
-    IpAddress::IpAddress(uint32_t hostOrderIp)
-    {
-    }
-    */
     IpAddress::IpAddress(uint32_t hostOrderIp)
         : _addrFamily(Family::IpV4)
+        , _data()
     {
         _data.ipV4Data = ::htonl(hostOrderIp);
     }
 
     IpAddress::IpAddress(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4)
         : _addrFamily(Family::IpV4)
+        , _data()
     {
         uint32_t hostOrderIp = static_cast<uint32_t>(byte1) << 24;
         hostOrderIp |= static_cast<uint32_t>(byte2) << 16;
@@ -131,22 +31,73 @@ namespace Infra
 
     IpAddress::IpAddress(const uint8_t* pAddr)
         : _addrFamily(Family::IpV6)
+        , _data()
     {
         ::memcpy(_data.ipV6Data, pAddr, IPV6_ADDR_SIZE_BYTE);
     }
 
-    bool IpAddress::operator==(const IpAddress& left, const IpAddress& right) const
+    IpAddress::Family IpAddress::GetFamily() const
+    {
+        return _addrFamily;
+    }
+
+    std::string IpAddress::ToString() const
+    {
+        switch (_addrFamily)
+        {
+            case Family::IpV4:
+            {
+                in_addr address{};
+                address.s_addr = _data.ipV4Data;
+
+                char resultBuffer[INET_ADDRSTRLEN];
+
+                ::inet_ntop(AF_INET, &address, resultBuffer, INET_ADDRSTRLEN);
+
+                return resultBuffer;
+            }
+            case Family::IpV6:
+            {
+                in6_addr address{};
+
+                ::memcpy(&address.s6_addr, _data.ipV6Data, IPV6_ADDR_SIZE_BYTE);
+
+                char resultBuffer[INET6_ADDRSTRLEN];
+
+                ::inet_ntop(AF_INET, &address, resultBuffer, INET6_ADDRSTRLEN);
+
+                return resultBuffer;
+            }
+        }
+
+        return {};
+    }
+
+    std::optional<IpAddress> IpAddress::TryParse(const std::string& str)
+    {
+        in_addr destinationV4 {};
+        if (::inet_pton(AF_INET, str.c_str(), &destinationV4) == 1)
+            return IpAddress(::ntohl(destinationV4.s_addr));
+
+        in6_addr destinationV6 {};
+        if (::inet_pton(AF_INET6, str.c_str(), &destinationV4) == 1)
+            return IpAddress{ reinterpret_cast<const uint8_t*>(&destinationV6.s6_addr) };
+
+        return std::nullopt;
+    }
+
+    bool operator==(const IpAddress& left, const IpAddress& right)
     {
         if (left._addrFamily != right._addrFamily)
             return false;
 
         switch (left._addrFamily)
         {
-            case Family::IpV4:
+            case IpAddress::Family::IpV4:
             {
                 return left._data.ipV4Data == right._data.ipV4Data;
             }
-            case Family::IpV6:
+            case IpAddress::Family::IpV6:
             {
                 const auto* leftData = reinterpret_cast<const uint64_t*>(left._data.ipV6Data);
                 const auto* rightData = reinterpret_cast<const uint64_t*>(right._data.ipV6Data);
@@ -157,20 +108,8 @@ namespace Infra
         return false;
     }
 
-    bool IpAddress::operator!=(const IpAddress& left, const IpAddress& right) const
+    bool operator!=(const IpAddress& left, const IpAddress& right)
     {
         return !(left == right);
-    }
-
-    IpAddress::Family IpAddress::GetFamily() const
-    {
-    }
-
-    std::string IpAddress::ToString() const
-    {
-    }
-
-    std::optional<IpAddress> IpAddress::TryParse(const std::string& str)
-    {
     }
 }
